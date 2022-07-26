8a7e65a5b9dd46d274a794c4b290d87cf23d26a7
sojib-Ahmed <sojibahmed.cpd@gmail.com>
2022-07-26 23:32:27 +0600

    react add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './component/header/header'
+
+function App() {
+  return (
+    <div className="App">
+
+      <header></header>
+    </div>
+    
+  );
+}
+
+export default App;
